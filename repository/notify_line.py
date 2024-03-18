7663f86c60555fb5353b5b459a97970e640dc6e0
irene3221 <irene18883@gmail.com>
2024-03-18 20:39:33 +0800

    separate preparing and sending notes

--- repository/notify_line.py
+++ repository/notify_line.py
@@ -1,13 +1,15 @@
 import requests
 
 class LineNotification:
+    channel_access_token = None
     def __init__(self, channel_access_token):
         self.channel_access_token = channel_access_token
 
-    def send_notification(self, user_id, message):
+    @classmethod
+    def prepare_notification(cls, user_id, message):
         url = "https://api.line.me/v2/bot/message/push"
         headers = {
-            "Authorization": "Bearer " + self.channel_access_token,
+            "Authorization": "Bearer " + cls.channel_access_token,
             "Content-Type": "application/json"
         }
         data = {
@@ -19,6 +21,10 @@ class LineNotification:
                 }
             ]
         }
+        return url, headers, data
+
+    @classmethod
+    def send_notification(cls, url, headers, data):
         try:
             response = requests.post(url, headers=headers, json=data)
             if response.ok:
@@ -27,5 +33,4 @@ class LineNotification:
                 print("Failed to send Line notification:", response.text)
         except Exception as e:
             print("Failed to send Line notification:", str(e))
-
 
