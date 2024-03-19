dd9dd405860c3550b15c13b854a88928ccde3fa7
irene3221 <irene18883@gmail.com>
2024-03-19 18:11:52 +0800

    let users decide lang

--- repository/lang.py
+++ repository/lang.py
@@ -1,29 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
 
-Base = declarative_base()
+from schema.database.lang import Lang
 
-class Notification(Base):
-    __tablename__ = 'notifications'
-
-    id = Column(Integer, primary_key=True)
-    message_key = Column(String)
-    message = Column(String)
-    language_id = Column(Integer, ForeignKey('languages.id'))
-
-engine = create_engine("mysql+pymysql://admin:1234@127.0.0.1:3306/be103")
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-class NotificationSystem:
-    def __init__(self, language):
-        self.language = language
-
-    def get_notification(self, message_key):
-        notification = session.query(Notification).filter_by(message_key=message_key, language_id=self.language).first()
-        return notification.message if notification else f"No message found for key '{message_key}'"
-
-
+def choose_lang(db: Session, lang_id: int):
+    return db.query(Lang).filter(Lang.id == lang_id).first()
